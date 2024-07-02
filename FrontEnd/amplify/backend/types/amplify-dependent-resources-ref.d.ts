8e6a18120b8f9097d82000dab60a829d2608c99f
kadenchien <kaden.chien@gmail.com>
2024-07-02 09:45:41 -0400

    added cognito authentication and amplify init

---
+++ FrontEnd/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "genaiamplifyb097d0c7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
